8e83cf0d4fbf7d2b85c8608be6745dc93c4fab70
Philip Woods <elzairthesorcerer@gmail.com>
2015-06-05 14:54:29 -0400

    Improved docstring for org-if-goto-first-heading

--- org-if-misc.el
+++ org-if-misc.el
@@ -6,7 +6,7 @@
 (setq lexical-binding t) ; Enable lexical binding
 
 (defun org-if-goto-first-heading ()
-  "Go to the line containing the first heading in the current buffer."
+  "Go to the line containing the first major heading in the current buffer."
   (goto-char (point-min))
   (while (not (equal "* " (buffer-substring-no-properties (line-beginning-position)
                                                           (+ 2 (line-beginning-position)))))
